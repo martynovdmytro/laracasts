3758c1c0e55f79975e6ac387f3dd194a1f3814d3
dmytromartynov <dmytro_martynov@icloud.com>
2022-06-24 09:02:47 +0300

    routes created. database filled.

---
+++ resources/views/post.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+    <x-slot name="content">
+        <article>
+            <h1>
+                {{ $post->title  }}
+            </h1>
+            <p>
+                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </p>
+
+
+
+            <div>
+                {!! $post->body !!}
+            </div>
+        </article>
+        <p>
+            <a href="/">Go back</a>
+        </p>
+
+    </x-slot>
+</x-layout>
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\File;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('components/layout');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('posts',[
+        'posts' => Post::latest()->with('category', 'author')->get()
+    ]);
 });
+
+Route::get('posts/{post:slug}', function (Post $post) {
+
+    return view('post', [
+        'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function(Category $category){
+
+    return view('posts', [
+            'posts' => $category->posts
+        ]
+    );
+
+
+});
+
+Route::get('authors/{author:username}', function(User $author){
+
+    return view('posts', [
+        'posts' => $author->posts
+        ]
+    );
+
+});
+
+
+
+
