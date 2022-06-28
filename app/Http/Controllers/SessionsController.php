f4a413414f075ba7572dd0050ad4aa2dbcfa226f
dmytromartynov <dmytro_martynov@icloud.com>
2022-06-28 16:52:09 +0300

    log in added

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,27 @@ use Illuminate\Http\Request;
 class SessionsController extends Controller
 {
 
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+         $attributes = request()->validate([
+             'email' => 'required|email',
+             'password' => 'required'
+         ]);
+
+         if (auth()->attempt($attributes)) {
+             return redirect('/')->with('success', 'Welcome Back!');
+         }
+
+         return back()
+             ->withInput()
+             ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+    }
+
     public function destroy()
     {
         auth()->logout();
