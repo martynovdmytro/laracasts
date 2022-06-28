f293e58757db0f835a6c33c394603d11bda1fd83
dmytromartynov <dmytro_martynov@icloud.com>
2022-06-28 20:15:43 +0300

    comments added

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,17 @@
+@props(['comment'])
+
+<article class="flex bg-gray-100 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
+            <time>{{ $comment->created_at }}</time>
+        </header>
+
+        <p>
+            {{ $comment->body }}
+        </p>
+    </div>
+</article>
