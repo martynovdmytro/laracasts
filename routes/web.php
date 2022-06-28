f55ae306a2617cdcfff91ad79f968e3efc29f421
dmytromartynov <dmytro_martynov@icloud.com>
2022-06-28 14:45:29 +0300

    registration form added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
@@ -21,6 +22,10 @@ use \Illuminate\Support\Facades\File;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
+
 
 
 
